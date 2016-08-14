cb64c922e68c4fe467843924f8c62b303cf5ed5e
James <jedwards@usagb.net>
2016-08-14 14:58:51 -0400

    small updates

--- app/router.js
+++ app/router.js
@@ -3,7 +3,6 @@ import * as Posts from './controllers/post_controller';
 import * as UserController from './controllers/user_controller';
 import { requireAuth, requireSignin } from './services/passport';
 
-
 const router = Router();
 
 router.get('/', (req, res) => {
